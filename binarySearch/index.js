23d4480eccf861911f706bb80a72d144bb01526f
Pavlo Netrebsky <pavlo_netrebsky@Pavlos-MacBook-Pro.local>
2018-07-02 22:10:18 +0300

    binary-search

--- binarySearch/index.js
+++ binarySearch/index.js
@@ -1,21 +1,25 @@
 
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
-function binarySearch(numberToCheck, minIndex, maxIndex){
-    let min = minIndex || 0;
-    let max = maxIndex || primes.length;
+function binarySearch(numberToCheck, minIndex = 0, maxIndex = primes.length){
+    if (minIndex > maxIndex) return -1;
 
-    if(min > max) return -1;
+    let guessedIndex = parseInt((minIndex + maxIndex)/2);
+    let guessedNumber = primes[guessedIndex];
 
-    let guessIndex = primes[(minIndex + maxIndex)/2];
-
-    if(guessIndex === numberToCheck){
-        return guessIndex;
+    if (guessedNumber === numberToCheck){
+        console.log('guessedIndex found', guessedIndex);
+        return guessedIndex;
+    }
+        
+    if (guessedNumber > numberToCheck){
+        console.log('guessedIndex', guessedIndex);
+        binarySearch(numberToCheck, minIndex, guessedIndex - 1);
     } else {
-        if(guessIndex > numberToCheck){
-            
-        } else {
-            
-        }
+        binarySearch(numberToCheck, guessedIndex + 1, maxIndex);
     }
 }
+
+let test1 = binarySearch(2);
+
+console.log(test1);
