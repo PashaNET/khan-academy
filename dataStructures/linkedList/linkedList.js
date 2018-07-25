a697cd0819c7f47304210518a227a0a8f368e97e
PashaNET <pavlo.netrebsky@gmail.com>
2018-07-25 19:41:31 +0300

    template for linkedList class

---
+++ dataStructures/linkedList/linkedList.js
@@ -0,0 +1,25 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.length = 0
+        this.head = null
+    }
+
+    // methods to implement:
+
+    // peekHead()
+    // size()
+    // add(value)
+    // addAt(index)
+    // remove(value)
+    // removeAt(index)
+    // indexOf(value)
+    // elementAt(index)
+    // isEmpty()
+}
