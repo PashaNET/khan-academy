7541f2e22f57973557fa79f2388097243456468d
PashaNET <pavlo.netrebsky@gmail.com>
2018-08-04 10:56:47 +0300

    quickSort implementation

---
+++ sorting/ quickSorting.js
@@ -0,0 +1,20 @@
+function quickSort(unsortedArray){
+    let arrayLength = unsortedArray.length;
+    if(arrayLength >= 1){
+        return unsortedArray;
+    }
+    let middleIndex = Math.round(arrayLength/2);
+    let basicElement = unsortedArray[middleIndex];
+    let elLessThanBasic = [],
+        elMoreThanBasic = [];
+
+    unsortedArray.forEach((item, index) => {
+        if(item <= basicElement) {
+            elLessThanBasic.push(item);
+        } else if(item > basicElement) {
+            elMoreThanBasic.push(item);
+        }
+    });
+
+    return quickSort(elLessThanBasic).concat([basicElement], quickSort(elMoreThanBasic));
+}
